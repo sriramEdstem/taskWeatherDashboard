0416cfb126b24bd7f1277fd9ac2c33f44d2d523d
MIDUNRAJL2000 <midun.raj@edstem.com>
2024-10-30 17:50:42 +0530

    bg image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import WeatherCard from "@/components/WeatherCard";
 import { weatherData } from "@/data/sample";
 import Link from "next/link";
+import Image from "next/image";
+import Window from "public/sky.jpg"
 
 export default function Home() {
   const weatherGrid = weatherData.map((city) => (
@@ -9,12 +11,20 @@ export default function Home() {
   ));
 
   return (
-    <div className=" text-center p-10">
+    <div className="">
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={Window}
+          alt="car factory"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
       <Link href={"/favourites"}>
-        <button className=" rounded-lg p-3 bg-yellow-500">
+      <button className=" rounded-lg p-3 bg-yellow-500">
           View favourties
         </button>
-      </Link>
+              </Link>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {weatherGrid}
       </div>
