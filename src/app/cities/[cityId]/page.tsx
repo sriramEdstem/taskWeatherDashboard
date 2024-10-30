bd4d67648c288c03a9c108ab3cad842e250e191b
MIDUNRAJL2000 <midun.raj@edstem.com>
2024-10-30 15:04:11 +0530

    details

--- src/app/cities/[cityId]/page.tsx
+++ src/app/cities/[cityId]/page.tsx
@@ -1,3 +1,61 @@
 export default function CityDetails() {
-  return <div>hi</div>;
+  return(
+     <div className="container mx-auto p-3 bg-amber-500 bg-cover text-center ">
+     
+<h1 className="text-2xl font-bold mb-4 text-white">tvm</h1>
+<div className="bg-white bg-opacity-70 shadow-md p-4 rounded-lg mb-4">
+  <h2 className="text-lg font-bold">Current Weather</h2>
+  <div className=" flex justify-between">
+    <div>
+      <p>Temparature:</p>
+      <p>Condition</p>
+    </div>
+    <div>
+      <p>Humidity</p>
+      <p>Wind Speed</p>
+    </div>
+  </div>
+  <div className="flex justify-between">
+    <p>Pressure:</p>
+    <p>Visibility:</p>
+  </div>
+</div>
+
+<div className="bg-white bg-opacity-75 shadow-md p-3 rounded-lg mb-4 ">
+  <h2 className="text-lg font-bold">Hourly Forecast</h2>
+  <table className="w-full">
+    <thead>
+      <tr>
+        <th>Time</th>
+        <th>Temperature</th>
+        <th>Condition</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Time</td>
+        <td>Temp</td>
+        <td>Condition</td>
+      </tr>
+    </tbody>
+  </table>
+
+</div>
+<div className="bg-white bg-opacity-70 shadow-md p-3 rounded-lg mb-4 ">
+  <h2 className="text-lg font-bold">
+    Forecast
+  </h2>
+  <div className="flex justify-between">
+          <p>Max Temp: C</p>
+          <p>Min Temp: C</p>
+        </div>
+        <div className="flex justify-between">
+          <p>Sunrise: </p>
+          <p>Sunset: </p>
+        </div>
+
+
+</div>
+  </div>
+  )
 }
---
+++ src/components/CityDetail.tsx
@@ -0,0 +1,28 @@
+interface City{
+
+}
+export default function CityDetail(){
+    return(
+        <div className="container mx-auto p-2">
+            <h1 className="mb-4">London</h1>
+
+            <div className="bg-white p-2 mb-2 shadow-md rounded-lg border">
+                <h2>Current Weather</h2>
+            </div>
+            <div className="flex justify-between">
+                <div>
+                    <p>Temparature:</p>
+                    <p>Condition of weather:</p>
+                </div>
+                <div>
+                    <p>Humidity:</p>
+                    <p>Wind Speed:</p>
+                </div>
+            </div>
+            <div className="flex justify-between">
+                <p>Pressure:</p>
+                <p>Visibility:</p>
+            </div>
+        </div>
+    )
+}
