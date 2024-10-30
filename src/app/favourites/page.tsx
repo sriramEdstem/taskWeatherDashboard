176e9ffeedbc264640664510a9b9da8e0982f79a
sriramEdstem <sriram.susheer@edstem.com>
2024-10-30 17:18:39 +0530

    condition for favourites page

--- src/app/favourites/page.tsx
+++ src/app/favourites/page.tsx
@@ -1,17 +1,25 @@
 "use client";
+
 import React from "react";
 import WeatherCard from "@/components/WeatherCard";
 import { useFavouriteStore } from "./_store";
 
 export default function Favourites() {
   const { favourites } = useFavouriteStore((state) => state);
-  const weatherGrid = favourites.map((city) => (
-    <WeatherCard key={city.cityId} city={city} />
-  ));
+
+  if (!favourites.length) {
+    return (
+      <div>
+        <h1>Favourites is empty</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {weatherGrid}
+      {favourites.map((city) => (
+        <WeatherCard key={city.cityId} city={city} />
+      ))}
     </div>
   );
 }
